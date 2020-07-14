04728ad5cff6e786408b07a086ff2ac50e63cf20
Levi2015 <67530820+Levi2015@users.noreply.github.com>
2020-07-14 00:21:21 -0600

    Add files via upload

---
+++ inicio.php
@@ -0,0 +1,24 @@
+<?php
+echo "BIENVENIDOS a pagina de Deudores";
+
+
+?>
+<html>
+<head>
+		<meta charset="UTF-8">
+	<title>Taller_levi</title>
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
+<link rel="stylesheet" href="css/bootstrap.min.css"> 
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+<form action="index.html">
+    <input type="submit" value="IR AL MENU" />
+</form>
+
+
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
